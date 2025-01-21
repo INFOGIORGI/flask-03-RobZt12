4dcd043bdd0c5a3c41b412fb07ec70427a01f2d2
Roberto Marzio <103176073+RobZt12@users.noreply.github.com>
2025-01-21 16:01:26 +0000

    Marzio3

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
+
+
+
+"""Creare applicazione web con due pagine:
+
+    home page con una navbar con due collegamenti (home page, details)
+    pagina details contenente una tabella con dei prodotti (nome prodotto, scaffale, prezzo), una navbar con tre collegamenti (home page, details e wikipedia.
+    ogni pagina ha un titolo.
+
+I dati dei prodotti sono memorizzati all'interno di una tupla di tuple."""
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html",titolo = 'home')
+@app.route("/details")
+def details():
+    return render_template("details.html",titolo = 'details')
 
-app.run()
+app.run(debug=True)
